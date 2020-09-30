da0eda5a9090b77071f3b73b365b0877aee4f5b4
Alperen Dikici <dikicialperen@gmail.com>
2020-09-30 22:43:03 +0200

    test class has been changed

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,9 @@
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Testing");
+
+        //command line 1
+        //command line 2
+        //command line 3
     }
 }
